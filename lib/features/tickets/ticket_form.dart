65a0a7f4c8c5e5f364781895c23c57a55488f4a0
RaymundoSantorski <rayma9829@gmail.com>
2026-08-10 12:49:57 -0600

    add: product selection on ticket form

--- lib/features/tickets/ticket_form.dart
+++ lib/features/tickets/ticket_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:tickets/core/models/product.dart';
+import 'package:tickets/features/tickets/ticket_item.dart';
+import 'package:tickets/features/tickets/ticket_product_selection_modal.dart';
 
 class TicketForm extends StatefulWidget {
   const TicketForm({super.key});
@@ -8,14 +11,81 @@ class TicketForm extends StatefulWidget {
 }
 
 class _TicketFormState extends State<TicketForm> {
+  final List<TicketItem> ticketProducts = [];
+
   @override
   Widget build(BuildContext context) {
+    void onAdd(TicketItem product) {
+      if (ticketProducts
+          .where((item) => item.productId == product.productId)
+          .isNotEmpty)
+        return;
+
+      setState(() {
+        ticketProducts.add(product);
+      });
+    }
+
+    void addUnits(int id) {
+      TicketItem item = ticketProducts
+          .where((item) => item.productId == id)
+          .first;
+      setState(() {
+        item.quantity = item.quantity + 1;
+        item.subtotal = item.quantity * item.unitPrice;
+      });
+    }
+
+    void deleteUnits(int id) {
+      TicketItem item = ticketProducts
+          .where((item) => item.productId == id)
+          .first;
+      if (item.quantity <= 0) return;
+      setState(() {
+        item.quantity = item.quantity - 1;
+        item.subtotal = item.quantity * item.unitPrice;
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text('Nuevo ticket')),
       body: ListView(
         children: [
           FilledButton(onPressed: () {}, child: Text('Seleccionar cliente')),
-          FilledButton(onPressed: () {}, child: Text('Agregar producto')),
+          ...ticketProducts.map((product) {
+            return Card(
+              child: Row(
+                children: [
+                  Text('${product.subtotal}'),
+                  SizedBox(width: 10),
+                  Text('${product.name} - ${product.code}'),
+                  Expanded(child: SizedBox(height: 40)),
+                  IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () => deleteUnits(product.productId),
+                  ),
+                  Text('${product.quantity}'),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () => addUnits(product.productId),
+                  ),
+                ],
+              ),
+            );
+          }),
+          FilledButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                ),
+                builder: (context) =>
+                    TicketProductSelectionModal(onSelected: onAdd),
+              );
+            },
+            child: Text('Agregar producto'),
+          ),
           FilledButton(onPressed: () {}, child: Text('Agregar descuento')),
           TextField(decoration: InputDecoration(label: Text('Notas'))),
           FilledButton(
---
+++ lib/features/tickets/ticket_product_selection_modal.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tickets/core/models/product.dart';
+import 'package:tickets/features/products/product_provider.dart';
+import 'package:tickets/features/tickets/ticket_item.dart';
+
+class TicketProductSelectionModal extends StatelessWidget {
+  const TicketProductSelectionModal({super.key, required this.onSelected});
+  final void Function(TicketItem) onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Product> products = context.watch<ProductProvider>().products;
+    debugPrint('${products.length}');
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.6,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              const Icon(Icons.shopping_bag, color: Colors.blueAccent),
+              const SizedBox(width: 10),
+              Text(
+                'Agregar productos',
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Expanded(child: Container()),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            child: ListView.separated(
+              itemCount: products.length,
+              separatorBuilder: (_, _) => const Divider(height: 1),
+              itemBuilder: (context, index) {
+                final product = products[index];
+                debugPrint(product.name);
+                debugPrint('${products.length}');
+                return ListTile(
+                  title: Text(
+                    product.name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  subtitle: Text(
+                    product.code,
+                    style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                  onTap: () {
+                    TicketItem item = TicketItem()
+                      ..code = product.code
+                      ..name = product.name
+                      ..productId = product.id
+                      ..quantity = 1
+                      ..subtotal = product.price
+                      ..unitCost = product.cost
+                      ..unitPrice = product.price;
+                    onSelected(item);
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
