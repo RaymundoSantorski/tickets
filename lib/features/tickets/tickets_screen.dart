711244fc938556a69421fb884e57e7a648badf7b
RaymundoSantorski <rayma9829@gmail.com>
2026-08-12 09:56:08 -0600

    add: ticket preview list

--- lib/features/tickets/tickets_screen.dart
+++ lib/features/tickets/tickets_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tickets/core/models/ticket.dart';
 import 'package:tickets/features/tickets/ticket_form.dart';
+import 'package:tickets/features/tickets/ticket_provider.dart';
 
 class TicketsScreen extends StatelessWidget {
   const TicketsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Ticket> tickets = context.watch<TicketProvider>().tickets;
     return Scaffold(
       appBar: AppBar(title: Text('Tickets'), actions: []),
       floatingActionButton: FloatingActionButton(
@@ -16,6 +20,44 @@ class TicketsScreen extends StatelessWidget {
           );
         },
         child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          Ticket ticket = tickets[index];
+          return Card(
+            color: ticket.status == TicketStatus.pending
+                ? Colors.redAccent
+                : ticket.status == TicketStatus.partial
+                ? Colors.amberAccent
+                : Colors.greenAccent,
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(ticket.displayName),
+                      SizedBox(height: 40),
+                      Text('${ticket.total}'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '${ticket.date.weekday} - ${ticket.date.day} - ${ticket.date.month} - ${ticket.date.year}',
+                      ),
+                      SizedBox(height: 40),
+                      Text(ticket.status.name),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        itemCount: tickets.length,
       ),
     );
   }
