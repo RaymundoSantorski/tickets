a0e187e0f458466c06aab90f562b98e1ee07f825
RaymundoSantorski <rayma9829@gmail.com>
2026-08-17 12:57:09 -0600

    delete: unnecesary import

--- lib/features/dashboard/dashboard_screen.dart
+++ lib/features/dashboard/dashboard_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:tickets/features/dashboard/widgets/dashboard_item.dart';
 
 class DashboardScreen extends StatelessWidget {
