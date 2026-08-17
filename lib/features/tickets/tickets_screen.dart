f495f3439d9b7e62316a40aa062d9050928f469b
RaymundoSantorski <rayma9829@gmail.com>
2026-08-17 12:57:44 -0600

    add: use of ticket card

--- lib/features/tickets/tickets_screen.dart
+++ lib/features/tickets/tickets_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tickets/core/models/ticket.dart';
+import 'package:tickets/features/tickets/ticket_card.dart';
 import 'package:tickets/features/tickets/ticket_details_screen.dart';
 import 'package:tickets/features/tickets/ticket_form.dart';
 import 'package:tickets/features/tickets/ticket_provider.dart';
@@ -25,47 +26,7 @@ class TicketsScreen extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (context, index) {
           Ticket ticket = tickets[index];
-          return InkWell(
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (_) => TicketDetailsScreen(ticket: ticket),
-                ),
-              );
-            },
-            child: Card(
-              color: ticket.status == TicketStatus.pending
-                  ? Colors.redAccent
-                  : ticket.status == TicketStatus.partial
-                  ? Colors.amberAccent
-                  : Colors.greenAccent,
-              child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(ticket.displayName),
-                        SizedBox(height: 40),
-                        Text('${ticket.balanceAfter}'),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          '${ticket.date.weekday} - ${ticket.date.day} - ${ticket.date.month} - ${ticket.date.year}',
-                        ),
-                        SizedBox(height: 40),
-                        Text(ticket.status.name),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          );
+          return ticketCard(ticket, context);
         },
         itemCount: tickets.length,
       ),
