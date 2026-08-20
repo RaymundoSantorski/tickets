86fb0cef1e79612b3dd2ebc812f83e3135436a2b
RaymundoSantorski <rayma9829@gmail.com>
2026-08-20 11:24:03 -0600

    add: helper functions to calculate pending items, weight and volumetric weight in dedicated file

---
+++ lib/features/tickets/helpers/calculate.dart
@@ -0,0 +1,19 @@
+import 'package:tickets/core/models/ticket_item.dart';
+
+double calculateWeight(List<TicketItem> items) {
+  return items.fold(
+    0.0,
+    (last, item) => (item.unitWeight ?? 0) * item.quantity + last,
+  );
+}
+
+double calculateVolWeight(List<TicketItem> items) {
+  return items.fold(
+    0.0,
+    (last, item) => (item.unitVolumetricWeight ?? 0) * item.quantity + last,
+  );
+}
+
+int calculatePendingItems(List<TicketItem> items) {
+  return items.fold(0, (last, item) => item.quantity + last);
+}
