ccf92d9079b8109e7a59fb86e31a3c051033ff78
RaymundoSantorski <rayma9829@gmail.com>
2026-09-14 11:24:43 -0600

    add: cancel shipment request option

--- lib/features/tickets/shipment_detail_screen.dart
+++ lib/features/tickets/shipment_detail_screen.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tickets/core/models/ticket.dart';
+import 'package:tickets/features/tickets/ticket_provider.dart';
 
-class ShipmentDetailScreen extends StatelessWidget {
+class ShipmentDetailScreen extends StatefulWidget {
   const ShipmentDetailScreen({super.key, required this.ticket});
   final Ticket ticket;
 
+  @override
+  State<ShipmentDetailScreen> createState() => _ShipmentDetailScreenState();
+}
+
+class _ShipmentDetailScreenState extends State<ShipmentDetailScreen> {
+  Ticket? ticket;
   String buildStateLabel(ShipmentStatus status) {
     switch (status) {
       case ShipmentStatus.preparing:
@@ -21,16 +29,46 @@ class ShipmentDetailScreen extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    super.initState();
+    TicketProvider db = context.read<TicketProvider>();
+    loadTicket(db, widget.ticket.id);
+    db.addListener(() {
+      loadTicket(db, widget.ticket.id);
+    });
+  }
+
+  Future<void> loadTicket(TicketProvider db, int ticketID) async {
+    Ticket? result = await db.get(ticketID);
+    if (mounted) {
+      setState(() {
+        ticket = result;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Future<void> cancelRequest() async {
+      if (widget.ticket.type != TicketType.shipment) return;
+      widget.ticket.shipmentStatus = ShipmentStatus.cancelled;
+      TicketProvider db = context.read<TicketProvider>();
+      await db.save(widget.ticket);
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text('Envio - ${ticket.displayName}')),
+      appBar: AppBar(title: Text('Envio - ${ticket?.displayName}')),
       body: ListView(
         children: [
           Card(
             child: Row(
               children: [
                 Text('Estado:'),
-                Text(buildStateLabel(ticket.shipmentStatus)),
+                Text(
+                  buildStateLabel(
+                    ticket?.shipmentStatus ?? ShipmentStatus.none,
+                  ),
+                ),
               ],
             ),
           ),
@@ -38,21 +76,25 @@ class ShipmentDetailScreen extends StatelessWidget {
             child: Row(
               children: [
                 Text('Fecha de solicitud:'),
-                Text(ticket.dueDate.toString()),
+                Text(ticket?.dueDate?.toString() ?? ''),
               ],
             ),
           ),
-          Card(child: Row(children: [Text('Peso:'), Text('${ticket.weight}')])),
+          Card(
+            child: Row(children: [Text('Peso:'), Text('${ticket?.weight}')]),
+          ),
           Card(
             child: Row(
-              children: [Text('Volumen:'), Text('${ticket.volWeight}')],
+              children: [Text('Volumen:'), Text('${ticket?.volWeight}')],
             ),
           ),
           Card(
             child: Row(
-              children: [Text('Cantidad:'), Text('${ticket.shipQuantity}')],
+              children: [Text('Cantidad:'), Text('${ticket?.shipQuantity}')],
             ),
           ),
+          FilledButton(onPressed: cancelRequest, child: Text('Cancelar')),
+          FilledButton(onPressed: () {}, child: Text('Continuar')),
         ],
       ),
     );
